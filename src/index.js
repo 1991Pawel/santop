a66af096bc56860b76b4aff7d41dac4281d99124
Pawel L <virtuozoo@o2.pl>
2021-05-06 23:25:23 +0200

    add mobileNav

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('fafa');
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav');
+
+const handleClick = () => {
+  hamburger.classList.toggle('hamburger--active');
+  nav.classList.toggle('nav--active');
+};
+
+hamburger.addEventListener('click', handleClick);
