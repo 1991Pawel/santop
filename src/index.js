69cfcb26055dabf741fcb135723b6566b1608453
Pawel L <virtuozoo@o2.pl>
2021-05-10 02:04:20 +0200

    add basic layout styles

--- src/index.js
+++ src/index.js
@@ -7,3 +7,31 @@ const handleClick = () => {
 };
 
 hamburger.addEventListener('click', handleClick);
+
+// gsap
+
+const tl = gsap.timeline({ defaults: { ease: 'power3.out' } });
+
+tl.fromTo(
+  '.overlay__logo',
+  { opacity: 0 },
+  { duration: 1, opacity: 1 },
+  '+=.5'
+);
+
+tl.to('.overlay', { y: '-100%', duration: 1, delay: 0.5 });
+tl.fromTo(
+  '.header',
+  { y: '-100%', opacity: 0 },
+  { y: '0', opacity: 1 },
+  '-=.3'
+);
+
+tl.fromTo('.hero__title', { y: '-10%', opacity: 0 }, { y: '0%', opacity: 1 });
+tl.fromTo(
+  '.hero__subtitle',
+  { y: '-10%', opacity: 0 },
+  { y: '0%', opacity: 1 }
+);
+
+tl.fromTo('.hero__cta', { y: '-10%', opacity: 0 }, { y: '0%', opacity: 1 });
